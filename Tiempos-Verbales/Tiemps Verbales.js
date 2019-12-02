5893b1e3773a018edf3828c82012f88044cf4db9
PsychoSatyr <54925848+PsychoSatyr@users.noreply.github.com>
2019-12-02 15:43:54 -0600

    idk

--- Tiempos-Verbales/Tiemps Verbales.js
+++ Tiempos-Verbales/Tiemps Verbales.js
@@ -1,4 +1,19 @@
 function OpenTab() {
     var Tabs = document.getElementById("Tab1");
-    Tabs.style.color = "red";
+    Tabs.style.color = "green";
+}
+
+function OpenTab() {
+    var Tabs = document.getElementById("Tab2");
+    Tabs.style.color = "green";
+}
+
+function OpenTab() {
+    var Tabs = document.getElementById("Tab3");
+    Tabs.style.color = "green";
+}
+
+function OpenTab() {
+    var Tabs = document.getElementById("Tab4");
+    Tabs.style.color = "green";
 }
