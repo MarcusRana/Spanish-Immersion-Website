56000fde879dbdae47a9c01f0ad090e62eb99bfe
PsychoSatyr <54925848+PsychoSatyr@users.noreply.github.com>
2019-12-02 16:02:37 -0600

    Fixed grammar tabs

--- Tiempos-Verbales/Tiemps Verbales.js
+++ Tiempos-Verbales/Tiemps Verbales.js
@@ -1,19 +1,19 @@
-function OpenTab() {
+function OpenTab1() {
     var Tabs = document.getElementById("Tab1");
     Tabs.style.color = "green";
 }
 
-function OpenTab() {
+function OpenTab2() {
     var Tabs = document.getElementById("Tab2");
     Tabs.style.color = "green";
 }
 
-function OpenTab() {
+function OpenTab3() {
     var Tabs = document.getElementById("Tab3");
     Tabs.style.color = "green";
 }
 
-function OpenTab() {
+function OpenTab4() {
     var Tabs = document.getElementById("Tab4");
     Tabs.style.color = "green";
 }
