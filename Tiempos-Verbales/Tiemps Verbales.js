e162d7095bd197e9bb26fb8f35aaaa643bcc92e8
PsychoSatyr <54925848+PsychoSatyr@users.noreply.github.com>
2019-12-11 00:01:27 -0600

    added to grammar html and changes grammar js

--- Tiempos-Verbales/Tiemps Verbales.js
+++ Tiempos-Verbales/Tiemps Verbales.js
@@ -1,19 +1,33 @@
-function OpenTab1() {
-    var Tabs = document.getElementById("Tab1");
-    Tabs.style.color = "green";
-}
+// function OpenTab1() {
+//     var Tabs = document.getElementById("Tab1");
+//     Tabs.style.color = "green";
+// }
 
-function OpenTab2() {
-    var Tabs = document.getElementById("Tab2");
-    Tabs.style.color = "green";
-}
+// function OpenTab2() {
+//     var Tabs = document.getElementById("Tab2");
+//     Tabs.style.color = "green";
+// }
 
-function OpenTab3() {
-    var Tabs = document.getElementById("Tab3");
-    Tabs.style.color = "green";
-}
+// function OpenTab3() {
+//     var Tabs = document.getElementById("Tab3");
+//     Tabs.style.color = "green";
+// }
 
-function OpenTab4() {
-    var Tabs = document.getElementById("Tab4");
-    Tabs.style.color = "green";
-}
+// function OpenTab4() {
+//     var Tabs = document.getElementById("Tab4");
+//     Tabs.style.color = "green";
+//     Tabs.TEXT_NODE
+// }
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-target]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        target.classList.add('active')
+    })
+})
