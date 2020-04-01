71b273930f4b3454560be8104a431f15a443d0b3
Marcus Rana <marcusrana01@gmail.com>
2020-03-31 21:57:24 -0500

    Organized Files into folders, added email system

---
+++ In Construction/thankyou.php
@@ -0,0 +1,3 @@
+<?php
+echo "Thank you! You should have recieved a verification email to the email address you entered"
+?>
--- PHP/querySQL.php
+++ PHP/querySQL.php
@@ -4,16 +4,27 @@
 
 <?php
 
-    $Array = $_POST['String']; 
-    $Tense = $_POST['Tense']; 
+    $Verb = $_POST['String']; 
+    $InputTense = $_POST['btnClickedValue']; 
+    echo '<br>';
+    echo '<br>';
+    $TestString = 'Correr';
+    $Test = substr($TestString, -2);
+    $Root = str_replace($Test, "", $TestString);
+    $reg = '/'. $Test .'/i';
+    $sql = 'SELECT * FROM regverbs;';
+    $results = mysqli_query($conn, $sql);
+    foreach($results as $result) {
+        $Term = $result['Term_Normal'];
+        $Ending = $result['Ending'];
+        $Tense = $result['Tense'];
+        if (preg_match($reg,$Ending)==1 and $InputTense==$Tense){
+            $Conjugate = str_ireplace("-", $Root, $Term);
+            echo $Conjugate, ' ' ,$result['Tense'],' ',$result['Pronoun_Normal'], '<br>' ;
+        }
+    }
+    $resultCheck = mysqli_num_rows($results);
 
-        echo $Word;
-        $sql = 'SELECT * FROM irregverbs WHERE Verb_Normal = $Word AND Tense = $Tense;';
-        $results = mysqli_query($conn, $sql);
-        $resultCheck = mysqli_num_rows($results);
-        echo $resultCheck;
-        echo $results;
-    
 ?>
 
 
